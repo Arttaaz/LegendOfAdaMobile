e5330b5397b0643c77c4721a641f3b09a4ccba2c
Alexis Gouzon <arttaaz@gmail.com>
2018-01-04 17:58:17 +0100

    dsfsdf

--- www/js/dialog.js
+++ www/js/dialog.js
@@ -1,16 +1,15 @@
-prompt(e) {
+function prompt(e) {
+
+    console.log(navigator.notification);
+
     function onPrompt(results) {
         e.setAttribute("value", results.input1);
-        alert("coucou");
     }
 
     var message = "tg";
     if(e.type == "text") {
-        navigator.notification.prompt('Entrez votre nom d\'utilisateur : ', onPrompt);
+        navigator.notification.prompt('Entrez votre nom d\'utilisateur : ', onPrompt, '');
     } else {
-        navigator.notification.prompt('Entrez votre mot de passe : ', onPrompt);
+        navigator.notification.prompt('Entrez votre mot de passe : ', onPrompt, '');
     }
-
-    navigator.notification.beep(2);
-
 }
